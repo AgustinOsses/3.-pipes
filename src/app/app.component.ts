da37048607cf8bd9379b5e885383527fbe110ea8
unknown <ossesagus@gmail.com>
2020-05-08 11:31:19 -0300

    Pipes Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  nombre = 'Agustin';
+  nombre2 ='claUdio agunStIn osses';
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  video: 'watch?v=UuTT3rOm3Vc';
+  activar:boolean=true;
+  hereo = {
+    nombre:"Logan",
+    clave:"Wolverine",
+    edad: 500,
+    direccion:{
+      calle:"Primera",
+      casa:"19"
+    }
+  }
+
+  valorDePromesa = new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve('llego la data'),3500)
+
+  });
+
+  fecha = new Date();
 }
